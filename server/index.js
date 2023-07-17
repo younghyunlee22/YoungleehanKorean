816f4c66e1e71b2d6fead689add5476b90148f21
Younghyun Lee <younghyunlee22@gmail.com>
2023-07-17 14:38:00 -0500

    register login

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import authRoutes from "./routes/auth.js";
+
+dotenv.config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB ERROR => ", err));
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api", authRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Node server is running on port ${port}`);
+});
